6e315934b12ee9ae9fc8ec9688ef0ceb73002fa8
Mohamed Zaki <mamadovzicx@gmail.com>
2023-01-13 04:15:21 +0200

    Added detect_os script

---
+++ .github/detect_os.py
@@ -0,0 +1,15 @@
+import sys
+
+def detect_os():
+    if sys.platform.startswith('win'):
+        return "windows"
+    elif sys.platform.startswith('linux'):
+        return "unix"
+    elif sys.platform.startswith('darwin'):
+        return "macos"
+    else:
+        return "null" 
+
+os = {"os", detect_os()}
+
+print(os)
