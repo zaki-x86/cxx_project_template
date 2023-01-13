e28564aee2e55490b7572ae0a2de87895868ffb1
Mohamed Zaki <mamadovzicx@gmail.com>
2023-01-13 22:58:02 +0200

    Created command_runner script

---
+++ scripts/command_runner.py
@@ -0,0 +1,32 @@
+import subprocess
+from argparse import ArgumentParser, Namespace
+
+
+def parse_cmd_args() -> Namespace:
+    # Create an ArgumentParser object
+    parser = ArgumentParser(description='Download CMake')
+
+    # Add arguments to the parser
+    parser.add_argument('--command', type=str, help='shell command', required=False)
+
+    # Parse the command-line arguments
+    args = parser.parse_args()
+    return args
+
+
+def command( cmd : str ):
+    # Split the command string into a list of arguments
+    args = cmd.split()
+    try:
+        # Run the command and capture the output
+        output = subprocess.run(args, capture_output=True, check=True)
+        # Print the output
+        print(output.stdout.decode())
+    except subprocess.CalledProcessError as e:
+        # Print the error message
+        print(e.stderr.decode())
+        
+        
+if __name__ == "__main__":
+    args = parse_cmd_args()
+    command(args.command)
