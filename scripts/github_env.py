6de51d59cbc156843dfa35b87fe1fa369f49b1cb
Mohamed Zaki <mamadovzicx@gmail.com>
2023-01-13 22:08:05 +0200

    Added export to github path function

--- scripts/github_env.py
+++ scripts/github_env.py
@@ -1,4 +1,11 @@
 import os
+from detect_os import detect_os
 
 GITHUB_WORKSPACE = os.environ.get('GITHUB_WORKSPACE')
 GITHUB_PATH = os.environ.get('GITHUB_PATH')
+
+def export_to_github_env(path):
+    if detect_os() == "windows":
+        path_seprator = ";"
+    elif detect_os() == "unix" | detect_os() == "macos":
+        path_seprator = ":"
